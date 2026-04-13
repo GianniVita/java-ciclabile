a1423a7df20db5e587c92d536d7bb3c9e2014e12
giannivitale <giavitale78@gmail.com>
2026-04-13 15:30:45 +0200

    Implement addElemento method to dynamically add elements to the numeri array

--- src/Ciclabile.java
+++ src/Ciclabile.java
@@ -25,5 +25,14 @@ public class Ciclabile {
         return element;
     }
 
+    void addElemento(int newNumber) {
+        int[] num = new int[numeri.length + 1];
 
+
+        for (int i = 0; i < numeri.length; i++) {
+            num[i] = numeri[i];
+        }   
+
+        num[num.length - 1] = newNumber;
+    }
 }
